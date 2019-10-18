214e73a1119cd39f40f67f883b494f7b7938ef88
Yariel1983 <pcprogrammer4all@gmail.com>
2019-10-18 13:03:14 -0400

    1 page done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component }
+from 'react';
 
 
 class App extends Component {
     render() {
         return (
-                //Navigation//
                 <div>
-                    <div className="container-fluid">
-                        <div className="row">
-                            <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">hello</div>
-                            <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                                <div className="vl">
-                                    <div className="container" id="test">
-                                        <h4 className="text-center" id="logName">eTags.com</h4>
-                                        <h2 className="text-center">Driven by Technology</h2>
+                    <div>
+                        <div className="container-fluid">
+                            <div className="row">
+                                <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                    <div className="container">  </div>                                      
+                                    <div className="container" id="button">
+                                        <h1>Renew Your Vehicle Registration</h1>
+                                        <h3>Enter Your Vehicle Information</h3>
+                                        <form>
+                                            <div className="form-group">
+                                                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="License Plate"/>
+                                            </div>
+                                            <div className="form-group">
+                                                <select className="form-control text-muted" id="exampleFormControlSelect1">
+                                                    <option>Vehicle Make</option>
+                                                    <option>Ford</option>
+                                                    <option>Honda</option>
+                                                    <option>Toyota</option>
+                
+                                                </select>
+                                            </div>                
+                                        </form>
+                                        <button type="button" class="btn btn-primary btn-lg btn-block">Next</button>
+                                    </div>
+                                </div>
+                
+                
+                
+                                <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                    <div className="vl">
+                                        <div className="container" id="test">
+                                            <h4 className="text-center" id="logName">eTags.com</h4>
+                                            <h2 className="text-center">Driven by Technology</h2>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
