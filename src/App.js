5ee568997af8ddf2e1521159f257058b34e2fc12
Yariel1983 <pcprogrammer4all@gmail.com>
2019-10-18 10:52:36 -0400

    React format

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+    render() {
+        return (
+                //Navigation//
+                <div>
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">hello</div>
+                            <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                <div className="vl">
+                                    <div className="container" id="test">
+                                        <h4 className="text-center" id="logName">eTags.com</h4>
+                                        <h2 className="text-center">Driven by Technology</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                );
+    }
+}
+;
+
+export default App;
